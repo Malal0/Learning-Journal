00c4d81cd6f0124e5754ab5159707bca6ee50a47
UncomfortableChai <mj1055119@gmail.com>
2023-02-10 04:56:53 -0800

    added some functionaltiy to the view more button

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //////////////////////////////
 //    VARIABLES
 //////////////////////////////
+const hiddenPosts = document.querySelectorAll('div[class="post hidden"]');
+const viewMoreBtn = document.querySelector('.view-more-btn');
 
 //////////////////////////////
 //    FUNCTIONS
 //////////////////////////////
+function handleClick(e) {
+    if (e.target.classList.contains("view-more-btn")) {
+        toggleHiddenPosts();
+    }
+}
+
+function toggleHiddenPosts() {
+    hiddenPosts.forEach(post => post.classList.toggle("hidden"));
+    viewMoreBtn.textContent = hiddenPosts[0].classList.contains("hidden") ? "View More" : "View Less";
+}
 
 //////////////////////////////
 //    EVENT LISTENER
 //////////////////////////////
+addEventListener("click", handleClick);
