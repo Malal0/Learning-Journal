8c14052e6686223810bacf9d35ed6a9e3a00c245
UncomfortableChai <mj1055119@gmail.com>
2023-02-12 09:56:32 -0800

    updated the image names and created a data javascript file

---
+++ data.js
@@ -0,0 +1,37 @@
+export const articles = [
+    {
+        image: "blog-1.svg",
+        title: "Blog one",
+        link: "/blog1.html"
+    },
+    {
+        image: "blog-2.svg",
+        title: "Blog two",
+        link: "/blog2.html"
+    },
+    {
+        image: "blog-3.svg",
+        title: "Blog three",
+        link: "/blog3.html"
+    },
+    {
+        image: "blog-4.svg",
+        title: "Blog four",
+        link: "/blog4.html"
+    },
+    {
+        image: "blog-5.svg",
+        title: "Blog five",
+        link: "/blog5.html"
+    },
+    {
+        image: "blog-6.svg",
+        title: "Blog six",
+        link: "/blog6.html"
+    },
+    {
+        image: "hero-image.svg",
+        title: "My new journey as a bootcamp student.",
+        link: "/blogpage.html"
+    }
+]
--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 //////////////////////////////
 //    VARIABLES
 //////////////////////////////
-const hiddenPosts = document.querySelectorAll('a[class="hidden"]');
+import { articles } from "/data.js"
+console.log(articles);
+const postContainer = document.querySelector('.posts-container');
+const posts = postContainer.children;
 const viewMoreBtn = document.querySelector('.view-more-btn');
+let nums = randomNonrepeatingNumbers(3);
+// hideSomePosts();
+const hiddenPosts = document.querySelectorAll('a[class="hidden"]');
 
 //////////////////////////////
 //    FUNCTIONS
 //////////////////////////////
-function handleClick(e) {
-    if (e.target.classList.contains("view-more-btn")) {
-        toggleHiddenPosts();
-    }
+function handleClick() {
+    toggleHiddenPosts();
 }
 
 function toggleHiddenPosts() {
-    hiddenPosts.forEach(post => post.classList.toggle("hidden"));
+    hiddenPosts.forEach(key => key.classList.toggle("hidden"));
     viewMoreBtn.textContent = hiddenPosts[0].classList.contains("hidden") ? "View More" : "View Less";
 }
+
+function randomNonrepeatingNumbers(length) {
+    let arr = [];
+
+    for (let i = 0; i < length; i++) {
+        let randomNum = Math.floor(Math.random() * 6);
+        arr.includes(randomNum) ? i-- : arr.push(randomNum);
+    }
+
+    return arr;
+}
+
+function hideSomePosts() {
+    console.log(nums);
+
+    for (let i = 0; i < nums.length; i++) {
+        posts[nums[i]].classList.add("hidden")
+    }
+}
+
 
 //////////////////////////////
 //    EVENT LISTENER
 //////////////////////////////
-addEventListener("click", handleClick);
+
+if (viewMoreBtn) {
+    viewMoreBtn.addEventListener("click", handleClick);
+}
+
+//////////////////////////////
+//    RENDER ARTICLES
+//////////////////////////////
+postContainer.innerHTML = articles.map(article => `
+<a href="${article.link}" target="_self">
+<article class="post">
+    <img class="post-image" alt="post image" src="images/${article.image}" />
+    <time class="post-date" date="2023-02-23">february 23, 2023</time>
+    <h2 class="post-title">${article.title}</h2>
+    <p class="post-subtext">I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+        After
+        several months of learning in the Frontend Developer Career Path.</p>
+</article>
+</a>
+`).join('');
