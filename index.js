91fc492a32753218991f479f317397ba3c7dc82b
UncomfortableChai <mj1055119@gmail.com>
2023-02-15 10:34:06 -0800

    cleaned up the javascript and css

--- index.js
+++ index.js
@@ -5,77 +5,61 @@ import { articles } from "/data.js"
 const postContainer = document.querySelector('.posts-container');
 const posts = postContainer.children;
 const viewMoreBtn = document.querySelector('.view-more-btn');
-const exclude = document.body.dataset.exclude;
-const filteredArticles = articles.filter(article => article.title !== exclude);
-let nums = randomNonrepeatingNumbers(6);
+const filteredArticles = articles.filter(article => article.title !== document.body.dataset.exclude);
+const sortedArticles = filteredArticles.sort((a, b) => {
+    const aDate = new Date(a.date);
+    const bDate = new Date(b.date);
+    return aDate < bDate
+});
+
+//////////////////////////////
+//    RENDER RECENT POSTS
+//////////////////////////////
+postContainer.innerHTML = sortedArticles.map(article => {
+    const date = new Date(`${article.date}`).toISOString().split("T")[0];
+    return `
+    <a href="${article.link}" target="_self">
+    <article class="post">
+    <img class="post-image" alt="post image" src="${article.image}" />
+    <time class="post-date" date="${date}">${article.date}</time>
+    <h2 class="post-title">${article.title}</h2>
+    <p class="post-subtext">I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+    After
+    several months of learning in the Frontend Developer Career Path.</p>
+    </article>
+    </a>
+    `}).join('');
+
+//////////////////////////////
+//    HIDE LAST POSTS
+//////////////////////////////
+hideLastPosts();
 
 //////////////////////////////
 //    FUNCTIONS
 //////////////////////////////
-function handleClick() {
-    toggleHiddenPosts();
-}
+
+// function handleClick() {
+//     toggleHiddenPosts();
+// }
 
 function toggleHiddenPosts() {
-    hiddenPosts.forEach(key => key.classList.toggle("hidden"));
-    viewMoreBtn.textContent = hiddenPosts[0].classList.contains("hidden") ? "View More" : "View Less";
-}
-
-function randomNonrepeatingNumbers(length) {
-    let arr = [];
-
-    for (let i = 0; i < length; i++) {
-        let randomNum = Math.floor(Math.random() * 6);
-        arr.includes(randomNum) ? i-- : arr.push(randomNum);
+    for (let i = 3; i < filteredArticles.length; i++) {
+        posts[i].classList.toggle("hidden");
     }
 
-    return arr;
+    viewMoreBtn.textContent = posts[3].classList.contains("hidden") ? "View More" : "View Less";
 }
 
-function hideSomePosts() {
-    for (let i = 3; i < randomizedArticles.length; i++) {
-        console.log(posts[i])
+function hideLastPosts() {
+    for (let i = 3; i < filteredArticles.length; i++) {
         posts[i].classList.add("hidden");
     }
 }
-
 
 //////////////////////////////
 //    EVENT LISTENER
 //////////////////////////////
 
-if (viewMoreBtn) {
-    viewMoreBtn.addEventListener("click", handleClick);
-}
-
-//////////////////////////////
-//    RENDER ARTICLES
-//////////////////////////////
-
-// const randomizedArticles = nums.map(num => filteredArticles[num]);
-const randomizedArticles = [];
-for (let i = 0; i < filteredArticles.length; i++) {
-    nums[i] < 7 ? randomizedArticles.push(filteredArticles[nums[i]]) : randomizedArticles.push(filteredArticles[6])
-}
-
-postContainer.innerHTML = randomizedArticles.map(article => {
-    const date = new Date(`${article.date}`).toISOString().split("T")[0];
-    return `
-<a href="${article.link}" target="_self">
-    <article class="post">
-        <img class="post-image" alt="post image" src="${article.image}" />
-        <time class="post-date" date="${date}">${article.date}</time>
-        <h2 class="post-title">${article.title}</h2>
-        <p class="post-subtext">I'm excited to start a new learning journey as a Scrimba Bootcamp student!
-            After
-            several months of learning in the Frontend Developer Career Path.</p>
-    </article>
-</a>
-`}).join('');
-
-//////////////////////////////
-//    HIDE SOME ARTICLES
-//////////////////////////////
-
-hideSomePosts();
-const hiddenPosts = document.querySelectorAll('a[class="hidden"]');
+// viewMoreBtn.addEventListener("click", handleClick);
+viewMoreBtn.addEventListener("click", toggleHiddenPosts);
